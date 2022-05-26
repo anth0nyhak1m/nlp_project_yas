90d7523e7b5ef87bd6e977218861ae88356bcef3
Sasha Filippova <oleksandrafilippova@oleksandras-air.lan>
2022-05-26 00:27:29 -0500

    finished helper functions to remove stop words and stem them; updated jp notebook and reran regression with the cleaned data

--- util.py
+++ util.py
@@ -1,12 +1,24 @@
 import pandas as pd
-import re
-#import spacy
+
+#imports to remove stop words 
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+stop = stopwords.words('english')
+
+#import to do word stemming
+from nltk.stem.snowball import EnglishStemmer
+
 
 def cleaning_data(true_df_path, fake_df_path):
     """
-    Helper function to pre-process data (lemmatization, tokenization, removal of punctation & stop words).
+    Helper function to pre-process data (lower case, remove article source, removal of punctation & stop words).
 
-    Returns a list of tokens 
+    Inputs:
+      true_df_path: (str) path to csv file containing articles labeled as 'True'
+      fake_df_path: (str) path to csv file containing articles labeled as 'False'
+
+    Returns a pd dataframe with clean strings
     """
     #load in data
     fake_df = pd.read_csv(fake_df_path, usecols= ["text"])
@@ -23,8 +35,27 @@ def cleaning_data(true_df_path, fake_df_path):
     data = true_df.append(fake_df).sample(frac=1).reset_index().drop(columns=['index'])
 
     #remove all punctuation & single letters
-    data['text'] = data['text'].replace(r'[^\w\s]', '', regex=True)
-    data['text'] = data['text'].replace(r'\s\w\s', '', regex=True)
+    data['text'] = data['text'].replace(r'[^\w\s]', ' ', regex=True).str.lower()
+    data['text'] = data['text'].replace(r'\s\w\s', ' ', regex=True)
+
+    #remove stop words
+    data['text'] = data["text"].apply(lambda words: ' '.join\
+                    (word for word in words.split() if word not in stop))
 
     return data
 
+
+def stemming(pd_series):
+    """
+    Converts words into stems and returns a string with stemmed words per observation.
+      
+    Input:
+      pd_series: (pd series) a pandas series where each observation represents an article in 
+        a string form
+    
+    Returns: (pd series)
+    """ 
+    stemmer = EnglishStemmer()
+    processed_series = pd_series.apply(lambda words: ' '.join(stemmer.stem(word) for word in words.split()))
+
+    return processed_series
